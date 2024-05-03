5cc33d07b93b06b9068cb7e7bd3c4df9d9dd4bc3
zhangqingguo <2959379020@qq.com>
2024-05-03 18:00:47 +0800

    Site updated: 2024-05-03 18:00:44

--- js/hightlight/codeCopy.js
+++ js/hightlight/codeCopy.js
@@ -1,5 +1,5 @@
 $(function () {
-    var $copyIcon = $('<i class="fa fa-copy code_copy" title="复制代码" aria-hidden="true">复制</i>');
+    var $copyIcon = $('<i class="fa fa-copy code_copy" title="复制代码" aria-hidden="true"></i>');
     $('.code-area').prepend($copyIcon);
 new ClipboardJS('.fa-copy', {
     target: function (trigger) {
